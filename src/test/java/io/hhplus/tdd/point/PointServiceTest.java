73f7630dc94947bce058b749050f69f5ffe923e9
kimyezzang97 <kimyezzang97@gmail.com>
2025-03-27 22:05:05 +0900

    test: test: 포인트 충전 테스트 작성

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -29,18 +29,120 @@ class PointServiceTest {
 
     @Test
     @DisplayName("ID로 회원을 조회할 때 존재하면 그 회원정보를 가져온다.")
-    void test(){
+    void ifExistSelectById() {
         // given
-        long id = 2L;
+        Long id = 2L;
+        Long point = 1000L;
         PointService pointService = new PointService(new UserPointTable());
-        UserPoint userPoint = new UserPoint(id, 1000, System.currentTimeMillis());
-        pointService.insertOrUpdate(userPoint);
+        pointService.insertOrUpdate(id, point, TransactionType.CHARGE);
 
         // when
         UserPoint result = pointService.selectById(id);
 
         // then : 기존 충전했던 1,000 포인트와 일치한다.
-        assertThat(result.point()).isEqualTo(userPoint.point());
+        assertThat(result.point()).isEqualTo(point);
+    }
+
+    /**
+     * 포인트 충전
+     */
+    @Test
+    @DisplayName("포인트는 0이하로 충전할 수 없다.")
+    void canNotInsertUnderZeroPoint() {
+        // given
+        Long id = 2L;
+        Long point = 0L;
+        PointService pointService = new PointService(new UserPointTable());
+
+        // when
+        Exception e = null;
+        try {
+            UserPoint result = pointService.insertOrUpdate(id, point, TransactionType.CHARGE);
+        } catch (Exception exception) {
+            e = exception;
+        }
+
+        // then
+        assert e.getMessage().contains("포인트는 0 이하로 충전 및 사용할 수 없습니다.");
+        assert e instanceof IllegalArgumentException;
+    }
+
+    @Test
+    @DisplayName("포인트 첫 충전시 100만 초과로 충전할 수 없다.")
+    void canNotInsertOverMillionPoint() {
+        // given
+        Long id = 2L;
+        Long point = 10000001L;
+        PointService pointService = new PointService(new UserPointTable());
+
+        // when
+        Exception e = null;
+        try {
+            UserPoint result = pointService.insertOrUpdate(id, point, TransactionType.CHARGE);
+        } catch (Exception exception) {
+            e = exception;
+        }
+
+        // then
+        assert e.getMessage().contains("포인트는 1,000,000 초과로 충전 및 사용할 수 없습니다.");
+        assert e instanceof IllegalArgumentException;
+    }
+
+    @Test
+    @DisplayName("기존 포인트와 추가한 포인트의 합이 100만포인트를 초과할 수 없다.")
+    void canNotInsertOverMillionTotalPoint(){
+        // given
+        long id = 10L;
+        long point = 100000L;
+        PointService pointService = new PointService(new UserPointTable());
+        pointService.insertOrUpdate(id, point, TransactionType.CHARGE);
+        long point2 = 500L;
+
+        // when
+        Exception e = null;
+        try {
+            UserPoint result = pointService.insertOrUpdate(id, point2, TransactionType.CHARGE);
+        } catch (Exception exception) {
+            e = exception;
+        }
+
+        // then
+        assert e.getMessage().contains("총 포인트는 1,000,000을 초과할 수 없습니다.");
+        assert e instanceof IllegalArgumentException;
+    }
+
+    @Test
+    @DisplayName("ID가 없는 신규회원도 포인트를 충전할 수 있다.")
+    void ifNotExistUserInsertPoint() {
+        // given
+        long id = 10L;
+        long point = 10000L;
+        PointService pointService = new PointService(new UserPointTable());
+
+        // when
+        pointService.insertOrUpdate(id, point, TransactionType.CHARGE);
+        UserPoint result = pointService.selectById(id);
+
+        // then : 신규 회원이 충전했던 10,000 포인트와 조회한 포인트가 일치한다.
+        assertThat(point).isEqualTo(result.point());
+    }
+
+    @Test
+    @DisplayName("기존 회원은 잔여포인트에서 더 충전할 수 있다.")
+    void ifExistUserInsertPoint() {
+        // given
+        long id = 10L;
+        long point = 10000L;
+        PointService pointService = new PointService(new UserPointTable());
+        pointService.insertOrUpdate(id, point, TransactionType.CHARGE);
+        long point2 = 500L;
+
+        // when
+        pointService.insertOrUpdate(id, point2, TransactionType.CHARGE);
+        UserPoint result = pointService.selectById(id);
+
+        // then
+        assertThat(point + point2).isEqualTo(result.point());
     }
 
 }
